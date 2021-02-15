a598790a08f4e2a871183296b63774ac96912293
RaulGal <55711111+RaulGal@users.noreply.github.com>
2021-02-15 17:23:47 +0100

    revision
    He pasado tambien el scss, pero no lo he borrado del components por si acaso

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,4 +14,50 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+    title = 'Proyecto';
+    title2 = "CRM DE RAUL";
+    
+    clients = [{name:"Raul", id: 1}, {name:"Nestor", id: 2}, {name:"Alfonso", id: 3}, {name:"Jorge", id: 4}, {name:"Eustaquio", id: 5}]
+  
+    newClient = {
+      name: "",
+      id: 0,
+    }
+  
+  
+    addClient(){
+      
+      var length = this.clients.length;
+      
+      this.newClient.id = length + 1;
+  
+      this.clients.push({id: this.newClient.id, name: this.newClient.name});
+    }
+  
 }
+// export class AppComponent 
+
+
+  //title = 'Proyecto';
+ // title2 = "CRM DE RAUL";
+  
+ // clients = [{name:"Raul", id: 1}, {name:"Nestor", id: 2}, {name:"Alfonso", id: 3}, {name:"Jorge", id: 4}, {name:"Eustaquio", id: 5}]
+
+ // newClient = {
+  //  name: "",
+  //  id: 0,
+ // }
+
+
+  //addClient(){
+    
+   // var length = this.clients.length;
+    
+  //  this.newClient.id = length + 1;
+
+    //this.clients.push({id: this.newClient.id, name: this.newClient.name});
+
+
+
+
